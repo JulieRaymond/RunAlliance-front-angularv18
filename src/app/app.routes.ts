ef35c35253ddc3f96f37e5cac09edfc4502c11ef
Julie Raymond <julie.raymond@pichet.com>
2024-12-11 10:38:09 +0100

    Added : homepage route and dashboard admin route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardAdminComponent} from "./features/dashboards/dashboard-admin/dashboard-admin.component";
+import {HomepageComponent} from "./features/home/homepage/homepage.component";
+import {AppLayoutComponent} from "./layout/app.layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes =
+  [
+    {path: '', component: HomepageComponent},
+    {
+      path: 'dashboard', component: AppLayoutComponent,
+      children: [{path: 'admin', component: DashboardAdminComponent}]
+    },
+  ];
