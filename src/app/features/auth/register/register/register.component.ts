51804963c404d1bc1a14fa95f5fab4e85fbda68c
Julie Raymond <julie.raymond@pichet.com>
2024-12-11 15:34:08 +0100

    Added : register component

--- src/app/features/auth/register/register/register.component.ts
+++ src/app/features/auth/register/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SharedModule} from "../../../../shared/shared.module";
+import {PasswordModule} from "primeng/password";
+import {LayoutService} from "../../../../layout/service/app.layout.service";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    SharedModule,
+    PasswordModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  userPassword: any;
+  userConfirmPassword: any;
 
+  constructor(public layoutService: LayoutService) {
+  }
 }
