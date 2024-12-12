114f96cb9c29ed2aa31a3e64da672d631d75b672
Julie Raymond <julie.raymond@pichet.com>
2024-12-12 18:03:13 +0100

    Fixed : banner closing

--- src/app/core/components/banner/banner.component.ts
+++ src/app/core/components/banner/banner.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {Ripple} from "primeng/ripple";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-banner',
   standalone: true,
   imports: [
     RouterLink,
-    Ripple
+    Ripple,
+    NgIf
   ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
 
+  // Variable pour gérer la visibilité de la bannière
+  isBannerVisible: boolean = true;
+
+  // Méthode pour fermer la bannière
+  closeBanner() {
+    this.isBannerVisible = false;
+  }
 }
