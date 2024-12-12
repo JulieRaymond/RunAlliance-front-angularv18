893566505284110cf083a0cecd68d7cd17153750
Julie Raymond <julie.raymond@pichet.com>
2024-12-12 12:36:23 +0100

    Updated: login and register button to go back to home

--- src/app/features/auth/register/register/register.component.ts
+++ src/app/features/auth/register/register/register.component.ts
@@ -2,14 +2,16 @@ import {Component} from '@angular/core';
 import {SharedModule} from "../../../../shared/shared.module";
 import {PasswordModule} from "primeng/password";
 import {LayoutService} from "../../../../layout/service/app.layout.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    SharedModule,
-    PasswordModule
-  ],
+    imports: [
+        SharedModule,
+        PasswordModule,
+        RouterLink
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
