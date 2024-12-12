11ce83c872f05cd8a13f23dc33b36c9727fe0832
Julie Raymond <julie.raymond@pichet.com>
2024-12-12 17:39:42 +0100

    Fixed : button

--- src/app/features/errors/error-page/error-page.component.ts
+++ src/app/features/errors/error-page/error-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {Ripple} from "primeng/ripple";
+import {ButtonDirective} from "primeng/button";
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    Ripple,
+    ButtonDirective
   ],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.scss'
