10c8bd79897e00cf60d40b2c296b79c0a1cd7367
Julie Raymond <julie.raymond@pichet.com>
2024-12-12 15:29:16 +0100

    Added: toggle dark or light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {AppLayoutModule} from "./layout/app.layout.module";
+import {DOCUMENT} from "@angular/common";
+import {InputSwitchModule} from "primeng/inputswitch";
+import {SharedModule} from "./shared/shared.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppLayoutModule],
+  imports: [RouterOutlet, AppLayoutModule, InputSwitchModule, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'RunAlliance-front-angularv18';
+
+  #document = inject(DOCUMENT);
+  isDarkMode = false;
+  toggleLightDark() {
+    const linkElement = this.#document.getElementById(
+      'theme-css',
+    ) as HTMLLinkElement;
+    if (linkElement.href.includes('assets/layout/styles/theme/lara-light-indigo/theme.css')) {
+      linkElement.href = 'assets/layout/styles/theme/lara-dark-indigo/theme.css';
+      this.isDarkMode = true;
+    } else {
+      linkElement.href = 'assets/layout/styles/theme/lara-light-indigo/theme.css';
+      this.isDarkMode = false;
+    }
+  }
+  display: boolean = false;
+
 }
