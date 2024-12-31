22090da2ef656300f21551cbe095b0f7b3670bff
Julie Raymond <julie.raymond@pichet.com>
2024-12-31 16:10:32 +0100

    Updated : trying to configure update of users in admin panel

--- src/app/core/components/crud-user-admin/crud-user-admin.component.ts
+++ src/app/core/components/crud-user-admin/crud-user-admin.component.ts
@@ -18,8 +18,8 @@ import {ManageUsersService} from "../../../shared/services/manage-users.service"
 export class CrudUserAdminComponent {
 
   users: User[] = [];
-  user: User = {id: 0, email: '', role: '', password: ''};  // Ajout du champ password
-  roles = ['admin', 'user'];
+  user: User = { id: 0, email: '', roles: [], password: '' };
+  roles = ['ROLE_ADMIN', 'ROLE_USER'];
   userDialog = false;
   deleteUserDialog = false;
   deleteUsersDialog = false;
@@ -38,13 +38,20 @@ export class CrudUserAdminComponent {
   }
 
   openNew() {
-    this.user = {id: 0, email: '', role: '', password: ''};  // Réinitialiser le mot de passe
+    this.user = {id: 0, email: '', roles: [], password: ''};  // Réinitialiser le mot de passe
     this.submitted = false;
     this.userDialog = true;
   }
 
-  editUser(user: User) {
-    this.user = {...user};
+  editUser(user: any) {
+    // Utilisation d'un 'any' pour accepter la structure de l'API (avec 'roles')
+    this.user = {
+      id: user.id,
+      email: user.email,
+      roles: user.roles && user.roles[0] ? user.roles[0] : '',
+      password: user.password || ''
+    };
+    console.log(this.user);
     this.userDialog = true;
   }
 
@@ -62,22 +69,27 @@ export class CrudUserAdminComponent {
     this.submitted = true;
 
     // Vérification si les champs sont valides
-    if (this.user.email && this.validEmail(this.user.email) && this.user.role && (this.user.password || this.user.id)) {
+    if (this.user.email && this.validEmail(this.user.email) && this.user.roles && (this.user.password || this.user.id)) {
+      const userToSave = { ...this.user };
+
+      // Si l'utilisateur existe déjà, on met à jour sans toucher au mot de passe
       if (this.user.id) {
-        // Si l'utilisateur existe déjà, on met à jour sans toucher au mot de passe
-        this.manageUsersService.updateUser(this.user.id, this.user).subscribe(response => {
+        delete userToSave.password;
+
+        this.manageUsersService.updateUser(this.user.id, userToSave).subscribe(response => {
           this.messageService.add({severity: 'success', summary: 'Succès', detail: 'Utilisateur mis à jour'});
           this.loadUsers();
         });
       } else {
         // Si l'utilisateur est nouveau, on crée l'utilisateur avec un mot de passe
-        this.manageUsersService.createUser(this.user).subscribe(response => {
+        this.manageUsersService.createUser(userToSave).subscribe(response => {
           this.messageService.add({severity: 'success', summary: 'Succès', detail: 'Utilisateur créé'});
           this.loadUsers();
         });
       }
+
       this.userDialog = false;
-      this.user = {id: 0, email: '', role: '', password: ''};  // Réinitialisation
+      this.user = {id: 0, email: '', roles: [], password: ''};  // Réinitialisation
     }
   }
 
@@ -115,7 +127,7 @@ export class CrudUserAdminComponent {
         life: 3000
       });
     });
-    this.user = {id: 0, email: '', role: ''};
+    this.user = {id: 0, email: '', roles: []};
   }
 
   onGlobalFilter(table: any, event: Event) {
@@ -124,7 +136,6 @@ export class CrudUserAdminComponent {
 
   loadUsers() {
     this.manageUsersService.getAllUsers().subscribe(users => {
-      console.log(users);
       this.users = users;
     });
   }
