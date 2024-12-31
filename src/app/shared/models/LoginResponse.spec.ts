f162dd2a4278079faf53369daf247973b8a6a031
Julie Raymond <julie.raymond@pichet.com>
2024-12-31 16:37:16 +0100

    Removed : tests

--- src/app/shared/models/LoginResponse.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoginResponse } from './LoginResponse';
-
-describe('Login', () => {
-  it('should create an instance', () => {
-    expect(new LoginResponse()).toBeTruthy();
-  });
-});
--- src/app/shared/models/run.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Run } from './run.model';
-
-describe('Run', () => {
-  it('should create an instance', () => {
-    expect(new Run()).toBeTruthy();
-  });
-});
