003264a23839f4528a1cf65e5ad8ade90d77f35e
Julie Raymond <julie.raymond@pichet.com>
2025-01-07 12:05:54 +0100

    Added : forgot password to back ok

--- src/app/core/components/forgot-password/forgot-password.component.ts
+++ src/app/core/components/forgot-password/forgot-password.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import {SharedModule} from "../../../shared/shared.module";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {PasswordService} from "../../../shared/services/password.service";
-import {Router} from "@angular/router";
+import {MessageService} from "primeng/api";
+import {timer} from "rxjs";
 
 @Component({
   selector: 'app-forgot-password',
@@ -14,32 +15,46 @@ import {Router} from "@angular/router";
 export class ForgotPasswordComponent {
 
   forgotPasswordForm: FormGroup;
-  errorMessage: string = '';
-  successMessage: string = '';
 
   constructor(
     private fb: FormBuilder,
     private passwordService: PasswordService,
-    private router: Router
+    private messageService: MessageService
   ) {
     this.forgotPasswordForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
     });
   }
-
   onSubmit(): void {
     if (this.forgotPasswordForm.valid) {
       const email = this.forgotPasswordForm.get('email')?.value;
       if (email) {
         this.passwordService.forgotPassword(email).subscribe({
-          next: response => {
-            this.successMessage = 'Un email de réinitialisation de mot de passe a été envoyé.';
-            this.errorMessage = '';
+          next: (response) => {
+            timer(600).subscribe(() => {
+              if (response.success) {
+                this.messageService.add({
+                  severity: 'success',
+                  summary: 'Succès',
+                  detail: response.message, // Affiche le message de succès
+                });
+              } else {
+                this.messageService.add({
+                  severity: 'error',
+                  summary: 'Erreur',
+                  detail: response.message, // Affiche le message d'erreur
+                });
+              }
+            });
           },
-          error: error => {
-            this.errorMessage = 'Une erreur est survenue. Veuillez réessayer.';
-            this.successMessage = '';
-          }
+          error: (error) => {
+            console.error('API Error Response:', error);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erreur',
+              detail: 'Impossible d\'envoyer l\'email. Veuillez réessayer.',
+            });
+          },
         });
       }
     }
